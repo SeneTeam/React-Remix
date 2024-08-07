2eef509176825b4ce17e9f9df71479371dbe77ba
SeneTeam <beaulahschwartingrgq60@gmail.com>
2024-08-07 18:16:43 +0200

    test

--- netlify/functions/server.js
+++
@@ -1,8 +0,0 @@
-const { createRequestHandler } = require("@netlify/remix-adapter");
-const build = require("@remix-run/dev/server-build");
-const process = require("node:process");
-
-exports.handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
--- public/build/_shared/remix_hmr-OYPBTTNX.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-LGQXC5YX.js";
-import "/build/_shared/chunk-5GUXQVXG.js";
-import "/build/_shared/chunk-73CLBT4D.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-OYPBTTNX.js.map
